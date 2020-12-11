f7952f126cd76b5599b0d922c2fdaa5fbad2cbc1
uladzimirsinitsa <uladzimirsinitsa@gmail.com>
2020-12-11 20:41:04 +0300

    pre beta

--- crawler.py
+++ crawler.py
@@ -1,13 +1,12 @@
-import requests
-import lxml
-# pip install requests
-# pip install lxml
-from bs4 import BeautifulSoup
-# pip install beautifulsoup4
+import requests  # pip install requests
+from bs4 import BeautifulSoup  # pip install beautifulsoup4
 
 
-def get_html(url):
-    html_code_url = requests.get(url)  # Response
+URL = "https://casinogoodorbad.com/"
+
+
+def get_html():
+    html_code_url = requests.get(URL)  # Response
     return html_code_url.text  # возращает HTML код
 
 
@@ -19,14 +18,32 @@ def get_all_links(html):
     for all_object in all_objects:
         a = all_object.get('href')  # строка
         links.append(a)
+        links = sorted(links)
+
     return links
 
 
 def main():
-    url = "https://casinogoodorbad.com/"
-    all_links = get_all_links(get_html(url))
+    all_links = get_all_links(get_html())
+    sorted_all_links = []
     for i in all_links:
+        if i[:1] == '/':
+            sorted_all_links.append(i)
+        if i[:5] == 'https':
+            sorted_all_links.append(i)
+        else:
+            continue
+    for i in sorted_all_links:
         print(i)
+
+# TODO сохранить список не вошедших данных
+# TODO дополнить адреса
+# TODO пропинговать адреса на ответ
+# TODO сохранить список адресов не casinogoodorbad.com
+# TODO удалить дубли
+
+
+
 
 
 if __name__ == '__main__':
